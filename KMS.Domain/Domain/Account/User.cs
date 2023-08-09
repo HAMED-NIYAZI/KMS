a4a4331aba7d91c8e7d0d3fb6a1484fea6c63ba3
Hamed Niyazi <hamed.niyazi5@gmail.com>
2023-08-09 08:40:20 +0330

    refactor

--- KMS.Domain/Domain/Account/User.cs
+++ KMS.Domain/Domain/Account/User.cs
@@ -2,7 +2,6 @@
 {
     public class User : BaseEntity
     {
-
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
         public string? UserName { get; set; }
@@ -18,7 +17,7 @@
         public string? PositionName { get; set; }
         public string? PositionId { get; set; }
         public string? ImagePath { get; set; }
-        public Guid?  GradeId { get; set; }
+        public Guid? GradeId { get; set; }
         public Guid? ChartId { get; set; }
 
 
