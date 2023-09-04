e59965c8df6a965a246a283eb5793c97b9ff0255
Hamed Niyazi <hamed.niyazi5@gmail.com>
2023-09-04 10:52:59 +0330

    fix bugs

--- KMSUI/Controllers/OrganizationController.cs
+++ KMSUI/Controllers/OrganizationController.cs
@@ -1,4 +1,6 @@
-﻿using KMS.Application.Services.OrganizationService;
+﻿using KMS.Application.Dtos.OrganizationDto;
+using KMS.Application.Services.OrganizationService;
+using KMSUI.ViewModels.Organization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace KMSUI.Controllers
@@ -7,19 +9,50 @@ namespace KMSUI.Controllers
     {
         private readonly IOrganizationService organizationService;
 
-		public OrganizationController(IOrganizationService organizationService)
-		{
-			this.organizationService = organizationService;
-		}
-
-		public async Task<IActionResult> OrganizationList()
+        public OrganizationController(IOrganizationService organizationService)
         {
-			var organizationTree = await organizationService.GetOrganizationTree();
-			ViewBag.OrganizationTree = organizationTree;
-			return View();
+            this.organizationService = organizationService;
+        }
+
+        public async Task<IActionResult> OrganizationCreate()
+        {
+            var organizationTree = await organizationService.GetOrganizationTree();
+            ViewBag.OrganizationTree = organizationTree;
+            return View();
+        }
+
+        [HttpPost("Create")]
+        public async Task<IActionResult> Create(OrganizationAddViewModel org)
+        {
+            //validation
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction(nameof(OrganizationCreate));
+            }
+
+
+            OrganizationDto dto = new OrganizationDto
+            {
+                Id = Guid.NewGuid(),
+                PersianTitle = org.PersianTitle,
+                SortingNumber = org.SortingNumber
+            };
+            if (org.ParentId != null && org.ParentId != Guid.Empty ) { dto.ParentId = org.ParentId; }
+
+
+            await organizationService.Add(dto);
+
+
+            return RedirectToAction(nameof(OrganizationCreate));
         }
 
 
 
-	}
+        public async Task<IActionResult> OrganizationEdit()
+        {
+            var organizationTree = await organizationService.GetOrganizationTree();
+            ViewBag.OrganizationTree = organizationTree;
+            return View();
+        }
+    }
 }
