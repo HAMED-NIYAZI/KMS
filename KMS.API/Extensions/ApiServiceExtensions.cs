0582984206c1b95e3bb0588ddf103264728f8eef
Hamed.Niazmand <hamed.niyazi5@gmail.com>
2024-02-05 17:51:21 +0330

    .

--- KMS.API/Extensions/ApiServiceExtensions.cs
+++ KMS.API/Extensions/ApiServiceExtensions.cs
@@ -3,19 +3,19 @@ using Microsoft.EntityFrameworkCore;
 
 namespace KMS.Api.Extensions
 {
- 
-
     public static class ApiServiceExtensions
     {
         public static IServiceCollection AddApplicationService(this IServiceCollection services, IConfiguration configuration)
         {
 
             //اضافه کردن کانتکست
+
             services.AddDbContext<KMSContext>(options => {
                 options.UseSqlServer(configuration.GetConnectionString("KMSConnectionString"));
             });
 
-           // services.AddScoped<ITokenService, TokenService>();
+           //services.AddScoped<ITokenService, TokenService>();
+
             return services;
         }
     }
